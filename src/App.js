118818509f123e8870fe767c914487d51b07249f
Andres Higuita <yecid3257@live.com>
2021-04-02 22:30:35 -0500

    solve issue

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.count = this.count.bind(this);
+    this.state = {
+      length: 0,
+    };
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onKeyUp={this.count}></textarea>
+        <div className="counter">{this.state.length}</div>
       </div>
     );
+  }
+  count(e) {
+    this.setState({ length: e.target.value.length });
   }
 }
 
